d0e3edaba6b9f9ab8d8e2d3e87a209ab620de62b
lamar davis <155438522+lamarDavisgta5@users.noreply.github.com>
2024-07-03 08:31:24 -0700

    Update matrix.hpp

--- matrix.hpp
+++ matrix.hpp
@@ -7,8 +7,10 @@
   std::vector<std::vector<double>> vec_2D_matrixs_lines_members( int , int , std::vector<double> );
   std::vector<std::vector<double>> vec_2D_matrix_columns_members ( int , int , std::vector<double> );
   std::vector<std::vector<double>> matrixs_input_wise ( std::vector<std::vector<double>> , int );
-  std::vector<double> matrix_answer ();
+  std::vector<std::vector<double>> matrix_answer ();
   std::vector<std::vector<double>> mulitply_two_matrix ( std::vector<std::vector<double>> , std::vector<std::vector<double>> , std::vector<std::vector<double>> , std::vector<std::vector<double>> , std::vector<std::vector<double>> , std::vector<std::vector<double>> );
   std::vector<std::vector<double>> sum_and_sub_two_matrix ( std::vector<std::vector<double>> , std::vector<double> , std::vector<double> , int );
-  std::string& quit_or_continue( std::vector<std::vector<double>> , int , int );
+  std::vector<std::vector<double>> matrixs_1_inputs( std::vector<std::vector<double>> ); 
+  int matrix_determination( std::vector<std::vector<double> > , std::vector<double> ); 
+  std::string quit_or_continue( std::vector<std::vector<double>> , int , int , int , int );
 # endif
