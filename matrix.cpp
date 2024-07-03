94d27a93f5dd2165659e8f2486cd3c848fef2e97
lamar davis <155438522+lamarDavisgta5@users.noreply.github.com>
2024-07-03 08:27:59 -0700

    Update matrix.cpp
    The matrix.cpp update :
    Now you can calculate sum , sub and multiply of two matrix.
    The determination of the matrix is still in development . But you 
    can calculate matrixs 2*2  determination.

--- matrix.cpp
+++ matrix.cpp
@@ -14,7 +14,7 @@ int main () {
   //start(); 
    
 
-  std::vector<double> new_matrix { matrix_answer() };
+  std::vector<std::vector<double>> new_matrix { matrix_answer() };
   
   # if 0
 
@@ -90,13 +90,32 @@ std::string choose_matrix_operation() {
 return input;
 }
 
-std::string& quit_or_continue( std::vector<std::vector<double>> the_new_matrix , int the_new_matrixs_lines , int the_new_matrixs_columns ) {
-  static std::string quit_or_continue {};
+std::string quit_or_continue( std::vector<std::vector<double>> the_new_matrix , int the_new_matrixs_lines , int the_new_matrixs_columns , int matrixs_determination , int str_to_int_for_matrixs_operations ) {
+  std::string quit_or_continue {};
+  next_line
+  
+  if ( str_to_int_for_matrixs_operations == 1 || str_to_int_for_matrixs_operations == 2 || str_to_int_for_matrixs_operations == 3 ) {
+    std::cout << "The new matrix is : " << '\n';
+
+    for ( int counter { 0 }; counter < the_new_matrixs_lines; ++ counter ) {
+      
+      for ( int nested_counter { 0 }; nested_counter < the_new_matrixs_columns; ++ nested_counter ) {
+        std::cout << the_new_matrix[ counter ][ nested_counter ] << '\t';
+
+      }
+
+      std::cout << '\n';
+
+
+    }
+  }
+
+  else {
+    std::cout << "The matrixs det is : " << matrixs_determination << '\n';
+  }
 
   next_line
   while ( quit_or_continue != "quit" && quit_or_continue != "continue" ) {
-    
-    
     next_line
     std::cout << "Enter continue : To do other matrix operation with the new matrix" << '\n';
     std::cout << "Enter quit : To skip the first part of the bulid " << '\n';
@@ -128,7 +147,7 @@ return -1;
 
 std::string int_to_str ( int input ) {
   std::unordered_map < int , std::string > int_to_str_changer {
-    { 1 ,  " sum " } , { 2 , " subtrack " } , { 4 ,  " determination " } , { 5 , " inverse " }
+    { 1 ,  "sum " } , { 2 , "subtrack " } , { 4 ,  "determination " } , { 5 , "inverse " }
  
   };
 
@@ -145,25 +164,29 @@ return "null";
 
 
 
-std::vector<double> matrix_answer() {
-
-  static std::vector< std::vector<double> > l_and_c {};
-  static std::vector < std::vector<double> > matrixs_wise {};
-  static std::vector<double> matrixs_1_wise {};
-  static std::vector<double> matrixs_2_wise {};
-  static std::vector<std::vector<double>> line_member_matrix_1 {}; 
-  static std::vector<std::vector<double>> line_member_matrix_2 {}; 
-  static std::vector<std::vector<double>> column_member_martix_1 {};  
-  static std::vector<std::vector<double>> column_member_martix_2 {};  
-  std::vector<std::vector<double>> new_matrixs_lines_and_columns {};
-  static std::vector<std::vector<double>> the_new_matrix {};
-  next_line
-  next_line 
+std::vector<std::vector<double>> matrix_answer() {
+  std::vector<std::vector<double>> matrix_output_for_bit_manipluation {};
 
   while ( true ) {
-    static std::string q_or_c {}; 
+    std::vector< std::vector<double> > l_and_c {};
+    std::vector < std::vector<double> > matrixs_wise {};
+    std::vector<std::vector<double>> m1 {};
+    std::vector<double> matrixs_1_wise {};
+    std::vector<double> matrixs_2_wise {};
+    std::vector<std::vector<double>> line_member_matrix_1 {}; 
+    std::vector<std::vector<double>> line_member_matrix_2 {}; 
+    std::vector<std::vector<double>> column_member_martix_1 {};  
+    std::vector<std::vector<double>> column_member_martix_2 {};  
+    std::vector<std::vector<double>> new_matrixs_lines_and_columns {};
+    double det {};
+    next_line
+    next_line 
+    std::string q_or_c {}; 
     std::string matrixs_opearation_that_user_want { choose_matrix_operation() };
     int string_to_intger_changer { str_to_int ( matrixs_opearation_that_user_want ) };  
+    static std::vector<std::vector<double>> the_new_matrix ( ( ( string_to_intger_changer == 4 || string_to_intger_changer == 5 )? 1 : 2 ) );
+
+    std::cout << string_to_intger_changer << '\n';
 
     switch ( string_to_intger_changer ) {
       case 1:
@@ -172,6 +195,7 @@ std::vector<double> matrix_answer() {
         l_and_c = number_of_the_lines_and_columns_of_the_matrix( string_to_intger_changer );
         matrixs_wise = matrixs_input_wise( l_and_c , string_to_intger_changer );
        
+
         for ( int counter { 0 }; counter < l_and_c[ 0 ][ 0 ] * l_and_c[ 0 ][ 1 ]; ++ counter ) {
           matrixs_1_wise.push_back( matrixs_wise[ 0 ][ counter ] );
 
@@ -183,14 +207,26 @@ std::vector<double> matrix_answer() {
         }
 
         the_new_matrix = sum_and_sub_two_matrix ( l_and_c , matrixs_1_wise , matrixs_2_wise , string_to_intger_changer );
-        q_or_c = quit_or_continue( the_new_matrix , l_and_c[ 0 ][ 0 ] , l_and_c[ 0 ][ 1 ] );
         break;
 
       case 2:
         next_line
         std::cout << "You entered mines " << '\n'; 
         l_and_c = number_of_the_lines_and_columns_of_the_matrix( string_to_intger_changer );
+        matrixs_wise = matrixs_input_wise ( l_and_c , string_to_intger_changer );
+        
+        for ( int counter { 0 }; counter < l_and_c[ 0 ][ 0 ] * l_and_c [ 0 ][ 1 ] ; ++ counter ) {
+          matrixs_1_wise.push_back( matrixs_wise [ 0 ][ counter ] );
 
+        }
+
+
+        for ( int counter { 0 }; counter < l_and_c[ 1 ][ 0 ] * l_and_c [ 1 ][ 1 ]; ++ counter ) {
+          matrixs_2_wise.push_back( matrixs_wise [ 1 ][ counter ] );
+
+        }
+
+        the_new_matrix = sum_and_sub_two_matrix ( l_and_c , matrixs_1_wise , matrixs_2_wise , string_to_intger_changer );
         break;
 
       case 3:
@@ -216,25 +252,50 @@ std::vector<double> matrix_answer() {
         column_member_martix_1 = vec_2D_matrix_columns_members( l_and_c[ 0 ][ 0 ] , l_and_c[ 0 ][ 1 ] , matrixs_1_wise );        
         column_member_martix_2 = vec_2D_matrix_columns_members( l_and_c[ 1 ][ 0 ] , l_and_c[ 1 ][ 1 ] , matrixs_2_wise ); 
         the_new_matrix = mulitply_two_matrix(  l_and_c , matrixs_wise , line_member_matrix_1 , line_member_matrix_2 , column_member_martix_1 , column_member_martix_2 );
-        q_or_c = quit_or_continue( the_new_matrix , the_new_matrix.size() , the_new_matrix[ 0 ].size() );
         break;
 
       case 4:
         next_line
         std::cout << "You entered det " << '\n';
+        l_and_c = number_of_the_lines_and_columns_of_the_matrix( string_to_intger_changer );
+        matrixs_wise = matrixs_input_wise( l_and_c , string_to_intger_changer );
+
+        for ( int counter { 0 }; counter < l_and_c.size(); ++ counter ) {
+          the_new_matrix[ counter ].resize( l_and_c[ 0 ][ 1 ] );
+
+        }
+
+
+        for ( int counter { 0 }; counter < l_and_c[ 0 ][ 0 ] * l_and_c[ 0 ][ 1 ]; ++ counter ) {
+          matrixs_1_wise.push_back( matrixs_wise[ 0 ][ counter ] );
+
+        }
+
+        det = matrix_determination( l_and_c , matrixs_1_wise );
         break;
 
       case 5:
         next_line
         std::cout << "You entered inverse " << '\n';
+        l_and_c = number_of_the_lines_and_columns_of_the_matrix( string_to_intger_changer );
+        matrixs_wise = matrixs_input_wise( l_and_c , string_to_intger_changer );
         break;
 
     }
 
+    
+    q_or_c = quit_or_continue( the_new_matrix , the_new_matrix.size() , the_new_matrix[ 0 ].size() , det , string_to_intger_changer );
+
+    if ( q_or_c == "quit" ) {
+      matrix_output_for_bit_manipluation = the_new_matrix;
+      break;
+    }
+    
+
   }
 
   
-return { 0 , 0 , 0 };
+return matrix_output_for_bit_manipluation;
 }
 
 
@@ -242,8 +303,8 @@ return { 0 , 0 , 0 };
 std::vector <std::vector<double> > number_of_the_lines_and_columns_of_the_matrix( int str_to_int_from_the_matrix_operation  ) {
   std::vector < std::vector<double> > lines_and_columns ( ( ( str_to_int_from_the_matrix_operation == 4 || str_to_int_from_the_matrix_operation == 5 )? 1 : 2 ) );
   std::string int_to_string { int_to_str( str_to_int_from_the_matrix_operation ) };
-  
-  std::cout << int_to_str << '\n';
+
+  std::cout << "size : " << lines_and_columns.size() << '\n';
 
   if ( str_to_int_from_the_matrix_operation == 3 ) {
     for ( int counter { 1 }; counter <= lines_and_columns.size(); ++ counter ) {
@@ -279,7 +340,7 @@ std::vector <std::vector<double> > number_of_the_lines_and_columns_of_the_matrix
 
   else if ( str_to_int_from_the_matrix_operation == 1 || str_to_int_from_the_matrix_operation == 2 ) {
     std::vector<double> inputs {};
-    
+
     for ( int counter { 0 }; counter < 2; ++ counter ) {
       std::cout << "Enter the " << ( ( counter == 0 )? "lines " : "columns " ) << "of both matrix to " << int_to_string << " : " << '\n';
       double input {};
@@ -298,21 +359,52 @@ std::vector <std::vector<double> > number_of_the_lines_and_columns_of_the_matrix
 
   }
 
+
   else {
-    ;
+
+    for ( int counter { 0 }; counter < lines_and_columns.size(); ++ counter ) {
+      lines_and_columns[ counter ].resize( 2 ); 
+
+    }
+
+
+    double inputs_for_error {}; 
+
+    while ( true ) {      
+      next_line
+      std::cout << "Enter the lines and columns of the square of the the matrix : " << '\n';  
+      double square_matrixs_lines_and_columns {};
+      std::cin >> square_matrixs_lines_and_columns;
+      inputs_for_error = square_matrixs_lines_and_columns;
+
+      if ( std::cin.fail() || inputs_for_error <= 0 || inputs_for_error - static_cast<int> (inputs_for_error ) != 0 ) {
+        std::cin.clear();
+        std::cin.ignore( std::numeric_limits<std::streamsize>::max() , '\n');
+        next_line
+        std::cout << "Wrong input " << '\n';
+        std::cout << "Please enter again " << '\n';
+      }
+
+      else{ 
+        break;
+      }
+
+    }
+
+    std::fill( lines_and_columns[ 0 ].begin() , lines_and_columns[ 0 ].end() , inputs_for_error );
+
   }
 
   next_line
 
   for ( int counter { 0 }; counter < lines_and_columns.size(); ++ counter ) {
     
-    for ( int nested_counter { 0 }; nested_counter < ( ( str_to_int_from_the_matrix_operation == 1 || str_to_int_from_the_matrix_operation == 2 || str_to_int_from_the_matrix_operation == 3 )? 2 : 1 ); ++ nested_counter ) {
+    for ( int nested_counter { 0 }; nested_counter < 2; ++ nested_counter ) {
       std::cout << lines_and_columns[ counter ][ nested_counter ] << '\t';
 
     }
 
     std::cout << '\n';
-
 
   }
 
@@ -321,25 +413,42 @@ return lines_and_columns;
 
 std::vector < std::vector<double> > matrixs_input_wise ( std::vector < std::vector<double> > number_of_the_lines_and_columns , int str_to_int_from_the_matrix_operation  ) {
   std::vector < std::vector<double> > matrixs_wise ( ( ( str_to_int_from_the_matrix_operation == 4 || str_to_int_from_the_matrix_operation == 5 )? 1 : 2 ) );
+  std::cout << "size : " << matrixs_wise.size() << '\n';
+  
   int No_lines { 1 };
   int No_columns { 0 }; 
 
   for ( int counter { 0 }; counter < matrixs_wise.size(); ++ counter ) {
     next_line
+    double input_for_error {};
 
-    for ( int nested_counter { 0 }; nested_counter < number_of_the_lines_and_columns[ counter ][ 0 ] * number_of_the_lines_and_columns[ counter ][ 1 ]; ++ nested_counter ) {
+    for ( int nested_counter { 0 }; nested_counter < number_of_the_lines_and_columns[ 0 ][ 0 ] * number_of_the_lines_and_columns[ 0 ][ 1 ]; ++ nested_counter ) {
       ++ No_columns;
-      again:
-      next_line
-      std::cout << "Enter wise " << ( ( counter == 0 )? " A " : " B " ) << No_lines << No_columns << " for " << counter + 1 <<"_matrix " << " : " << '\n';
-      double wise {};
-      std::cin >> wise;
-
-      if ( wise - static_cast<int> ( wise ) != 0 ) {
+      while ( true ) {
         next_line
-        std::cout << "Wrong input " << '\n';
-        std::cout << "Please enter again " << '\n';
-        goto again;
+        std::cout << "Enter wise " << ( ( counter == 0 )? " A " : " B " ) << No_lines << No_columns << " for " << counter + 1 <<"_matrix " << " : " << '\n';
+        double wise {};
+        std::cin >> wise;
+
+        if (std::cin.fail() ) {
+            std::cin.clear(); // Clear the error flag
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore invalid input
+            next_line
+            std::cout << "Wrong input. Please enter a valid integer." << '\n';
+        }
+
+        if ( wise - static_cast<int> ( wise ) != 0 ) {
+          next_line
+          std::cout << "Wrong input " << '\n';
+          std::cout << "Please enter again " << '\n';
+
+        }
+
+        else { 
+          input_for_error = wise;
+          break;
+        }
+
       }
 
       if ( No_columns ==  number_of_the_lines_and_columns[ counter ][ 1 ] ) {
@@ -347,7 +456,7 @@ std::vector < std::vector<double> > matrixs_input_wise ( std::vector < std::vect
         No_columns = 0;
       }
       
-      matrixs_wise[ counter ].push_back( wise ); 
+      matrixs_wise[ counter ].push_back( input_for_error ); 
 
     }
 
@@ -359,8 +468,6 @@ std::vector < std::vector<double> > matrixs_input_wise ( std::vector < std::vect
 
 return matrixs_wise;
 }
-
-//std::vector<double> multiplication_operation_of_the_matrix
 
 
 
@@ -622,8 +729,6 @@ std::vector<std::vector<double>> mulitply_two_matrix ( std::vector< std::vector<
     next_line
     std::vector<std::vector<double>> C_M_V_O_N_M { classified_multiplication_values_of_new_matrix ( m_values , lines_and_columns[ 0 ][ 0 ] , lines_and_columns[ 0 ][ 1 ] , lines_and_columns[ 1 ][ 0 ] , lines_and_columns[ 1 ][ 1 ] ) };
     new_matrix_by_mulitiply_matrix_1_to_matrix_2 = the_new_matrix( C_M_V_O_N_M , lines_and_columns[ 0 ][ 0 ] , lines_and_columns[ 1 ][ 1 ] );
-    
-
 
   }
 
@@ -700,4 +805,141 @@ std::vector<std::vector<double>> mulitply_two_matrix ( std::vector< std::vector<
 
 
 return { { 0 , 0 } , { 0 , 0 } };
+}
+
+/*
+ 1 2
+ 3 4 
+
+ 1 == 0 , 2 == 1 , 3 == 2 , 4 == 3 
+
+ ( 1*4 ) - ( 2 * 3 ) == -2
+
+
+
+*/
+
+double detarmination_for_2_by_2_matrix ( std::vector<double> matrix_wise ) {
+  double det { ( matrix_wise[ 0 ] * matrix_wise [ 3 ] ) - ( matrix_wise[ 1 ] * matrix_wise[ 2 ] ) };
+return det;
+}
+
+std::vector<std::vector<double>> vec_main_diameter_of_double_matrix ( std::vector<std::vector<double>> wises ) {
+
+}
+
+std::vector<std::vector<double>> vec_side_diameter_of_double_matrix ( std::vector<std::vector<double>> wises ) {
+
+  
+}
+
+double matrixs_detarmination_in_rule_of_sarrus ( std::vector<double> matrixs_wise , std::vector<std::vector<double>> lines_and_columns_of_the_matrix ) {
+  std::vector<std::vector<double>> double_matrix ( 2 );  
+  int counter_for_matrixs_wise {};
+
+  for ( int counter { 0 }; counter < double_matrix.size(); ++ counter ) {
+    
+    for ( int nested_counter { 0 }; nested_counter < lines_and_columns_of_the_matrix[ 0 ][ 0 ] * lines_and_columns_of_the_matrix[ 0 ][ 1 ]; ++ nested_counter ) {
+      double_matrix[ counter ].push_back( matrixs_wise[ counter_for_matrixs_wise ] );   
+      ++ counter_for_matrixs_wise;
+    }
+
+    counter_for_matrixs_wise = 0;
+
+  }
+
+  next_line
+
+  for ( int counter { 0 }; counter < double_matrix.size(); ++ counter ) {
+    
+    for ( int nested_counter { 0 }; nested_counter < lines_and_columns_of_the_matrix[ 0 ][ 0 ] * lines_and_columns_of_the_matrix[ 0 ][ 1 ]; ++ nested_counter ) {
+      std::cout << double_matrix[ counter ][ nested_counter ] << '\t';
+    }
+    
+    std::cout << '\n';
+
+  }
+
+
+
+}
+
+double matrixs_detarmination_expantion_mathod() {
+
+  
+}
+
+
+
+double matrix_detrmination_operation( std::vector<std::vector<double> > square_matrixs_lines_and_columns , std::vector<double> matrixs_wise ) {
+  double det {};
+
+  if ( square_matrixs_lines_and_columns[ 0 ][ 0 ] == 1 ) {
+    det = matrixs_wise[ 0 ];
+  }
+
+  else {
+    
+    if ( square_matrixs_lines_and_columns[ 0 ][ 0 ] == 2 ) {
+      det = detarmination_for_2_by_2_matrix( matrixs_wise );
+  
+    }
+
+    else if ( square_matrixs_lines_and_columns[ 0 ][ 0 ] == 3 ) {
+      matrixs_detarmination_in_rule_of_sarrus( matrixs_wise , square_matrixs_lines_and_columns );
+
+    }
+
+    else {
+
+
+
+    }
+ 
+
+
+
+
+  }
+
+return det;
+}
+
+
+
+int matrix_determination( std::vector<std::vector<double> > square_matrixs_lines_and_columns , std::vector<double> matrixs_wise ) {
+  next_line
+  std::vector<std::vector<double>> matrix_in_vec_2D ( square_matrixs_lines_and_columns[ 0 ][ 1 ] );
+  int counter_for_matrixs_wise { 0 };
+
+  for ( int counter { 0 }; counter <  matrix_in_vec_2D.size(); ++ counter ) {
+    
+    for ( int nested_counter { 0 }; nested_counter < square_matrixs_lines_and_columns[ 0 ][ 1 ]; ++ nested_counter ) {
+      matrix_in_vec_2D[ counter ].push_back( matrixs_wise[ counter_for_matrixs_wise ] );
+
+    }
+    
+    std::cout << '\n';
+
+  }
+
+  counter_for_matrixs_wise = 0;
+
+  std::cout << "The matrix is : " << '\n';
+  next_line
+ 
+  for ( int counter { 0 }; counter < square_matrixs_lines_and_columns[ 0 ][ 0 ]; ++ counter ) {
+    
+    for ( int nested_counter { 0 }; nested_counter < square_matrixs_lines_and_columns[ 0 ][ 1 ]; ++ nested_counter ) {
+      std::cout << matrixs_wise[ counter_for_matrixs_wise ] << '\t';
+      ++ counter_for_matrixs_wise;
+    }
+
+    std::cout << '\n';
+  }
+
+  double det { matrix_detrmination_operation( square_matrixs_lines_and_columns , matrixs_wise ) };
+
+
+return det;
 }
